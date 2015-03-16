ece1495cae4fb616f8c57ff3816f3e09d2cd7488
Matthew Moisen <mmoisen@cisco.com>
2015-03-15 19:02:57 -0700

    ds18b20

--- test_ds18b20.py
+++ test_ds18b20.py
@@ -6,13 +6,18 @@ import os
 
 from models import Probe
 
-if len(sys.argv) == 1:
-    probes = [Probe('hlt', file) for file in os.listdir(Probe.BASE_DIR) if file != 'w1_bus_master1']
-else:
-    probes = [Probe('hlt', file) for file in sys.argv[1:]]
+
+
+def probes():
+    if len(sys.argv) == 1:
+        probes = [Probe('hlt', file) for file in os.listdir(Probe.BASE_DIR) if file != 'w1_bus_master1']
+    else:
+        probes = [Probe('hlt', file) for file in sys.argv[1:]]
+
+    return probes
 
 while True:
-    for probe in probes:
+    for probe in probes():
         try:
             print "{} temperature is {}".format(probe.file_name, probe.temp)
         except Exception as ex:
