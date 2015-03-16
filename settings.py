67b978654afa66c352a1a341efcad87c0657adda
Matthew Moisen <mmoisen@cisco.com>
2015-03-15 17:21:16 -0700

    settings

---
+++ settings.py
@@ -0,0 +1,29 @@
+__author__ = 'mmoisen'
+
+import peewee
+
+#MYSQL_USERNAME='username'
+#MYSQL_DATABASE='database'
+#MYSQL_PASSWORD='password'
+#MYSQL_HOST='host'
+
+'''
+todo:
+change this to a database proxy to use different dbs
+'''
+
+try:
+    from local_settings import *
+except ImportError:
+    print "Create a 'local_settings.py' file (or edit settings.py) containing the following constants:\nMYSQL_USERNAME=''\nMYSQL_HOST=''\nMYSQL_PASSWORD=''\nMYSQL_DATABASE=''\n"
+
+db = peewee.MySQLDatabase(MYSQL_DATABASE,user=MYSQL_USERNAME, host=MYSQL_HOST,passwd=MYSQL_PASSWORD)
+
+def get_db():
+    db.connect()
+    db.get_conn().ping(True)
+    return db
+
+class BaseModel(peewee.Model):
+    class Meta:
+        database = db
